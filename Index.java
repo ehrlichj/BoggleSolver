67c338d0f0f03b696025a3dfbe58ffb9b664fc5b
ehrlichj <34730207+ehrlichj@users.noreply.github.com>
2021-01-12 16:22:17 -0500

    added full dictionary

--- Index.java
+++ Index.java
@@ -1,19 +1,38 @@
+import java.io.*;
+import java.util.*;
 public class Index{
 
     public static void main(String[] args){
 
         //Array represantion of the global dictionary.
-        String[] dictionary = new String[]{"HELLOWORLD", "RABID", "GEEKS","FOR","QUIZ","GO","GUESS","GEEK","BOY","A","AND","REAR","RARE","BOAR","ROBIN"};
+        //String[] dictionary = new String[]{"HELLOWORLD", "RABID", "GEEKS","FOR","QUIZ","GO","GUESS","GEEK","BOY","A","AND","REAR","RARE","BOAR","ROBIN"};
         
         //Create Trie for dictionary.
         Trie_Node root = new Trie_Node('*');
         Trie myTrie = new Trie(root);
 
+        try{
+            File words = new File("words.txt");
+            Scanner myReader =new Scanner(words);
+
+        while(myReader.hasNextLine()){
+            String data = myReader.nextLine();
+            if(data.length()>=3){
+                Index.addWord(myTrie, data);
+            }
+        }
+
+        myReader.close();
+        }catch(FileNotFoundException e){
+            System.out.println("An error occured");
+            e.printStackTrace();
+        }
+
 
         //add words to the Trie dictionary
-        for(int i=0;i<dictionary.length; i++){
-            myTrie.add(dictionary[i]);
-        }
+       // for(int i=0;i<dictionary.length; i++){
+         //   myTrie.add(dictionary[i]);
+        //}
 
 
         //Create board object.
@@ -30,5 +49,9 @@ public class Index{
         int ascii =  letter;
         int letter_index = ascii-65;
         return letter_index;
+    }
+
+    public static void addWord(Trie trie, String word){
+        trie.add(word);
     }
 }
